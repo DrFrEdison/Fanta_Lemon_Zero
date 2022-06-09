2353fa2d1feed80b0774c41133a11534add1d03d
MPK <kurtz@mailbox.org>
2022-06-09 14:53:57 +0200

    start tta val

--- Rsource_Fanta_Lemon_Zero_val_V01.R
+++ Rsource_Fanta_Lemon_Zero_val_V01.R
@@ -9,10 +9,8 @@ setwd( print( this.path::this.dir() ) )
 setwd("..")
 dt$wd.git <- print( getwd() )
 
-# .opendir(dt$wd)
-
-dt$para$location = "Moenchengladbach"
-dt$para$line = "G9"
+dt$para$location = c("Moenchengladbach", "Genshagen")
+dt$para$line = c("G9", "G6")
 dt$para$main = paste0(dt$para$beverage, " in ", dt$para$location, ", line ", dt$para$line)
 dt$para$model.date <- c("220530")
 dt$para$model.pl <- c("00300")
@@ -20,13 +18,28 @@ dt$para$wl1 <- c(190)
 dt$para$wl2 <- c(598)
 dt$para$wl[[1]] <- seq(dt$para$wl1, dt$para$wl2, 1)
 
-dt$para$substance <- c("Acid", "Acesulfam", "Aspartam", "T1", "T2B", "T2")
+dt$para$substance <- c("TTA", "Acesulfam", "Aspartam", "T1", "T2B", "T2")
 dt$para$unit <- c( bquote("%"),  bquote("%"),  bquote("%"), bquote("%"), bquote("%"), bquote("%"))
-dt$para$ylab <- c( bquote("Acid in %"), bquote("Acesulfam in %"), bquote("Aspartam in %"), bquote("T1 in %"), bquote("T2B in %"), bquote("T2 in %") )
+dt$para$ylab <- c( bquote("TTA in %"), bquote("Acesulfam in %"), bquote("Aspartam in %"), bquote("T1 in %"), bquote("T2B in %"), bquote("T2 in %") )
 # dt$para$mop.date <- "220522"
 dt$para$SOLL <- c(100, 100,100,100,100,100)
 
-# #rename R files (run only once)
+dt$para$val.date <- "220609"
+
+# Linearity
+# setwd(dt$wd)
+# setwd("./Modellvalidierung")
+# setwd("./Linearitaet")
+# dt$lin$raw <- read.csv2( "220602_Max_Cherry_Linearitaet_TA_Acesulfam_Aspartam_Coffein.csv" , sep = "\t")
+# dt$lin$raw <- dt$lin$raw[ order(dt$lin$raw$Dilution) , ]
+# dt$lin$trs <- transfer_csv(dt$lin$raw)
+
+dt$para$eingriff <- c(2, 2, 2, 2)
+dt$para$sperr <- c(3, 3, 3, 3)
+
+dt$para$Charge <- c("0010905033", "0010987125", "0010873303", "0010987125")
+dt$para$Charge.Sirup <- "2206010745"
+# rename R files (run only once)
 # dt$para$Rfiles <- list.files(getwd(), pattern = ".R$", recursive = T)
 # file.rename(dt$para$Rfiles, gsub("beverage", dt$para$beverage, dt$para$Rfiles))
-# 
+
